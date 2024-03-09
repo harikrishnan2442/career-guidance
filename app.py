6d73e6f2d49f33152d60383d75b33cc0918d81bd
NormanOsborm <159689265+NormanOsborm@users.noreply.github.com>
2024-03-09 17:56:17 +0530

    added templates

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('user/404.html'), 404
